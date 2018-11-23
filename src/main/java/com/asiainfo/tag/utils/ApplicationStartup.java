fa3c781b3ad7753a157c4072531dba449fce0058
king-pan <pwpw1218@gmail.com>
2018-11-23 10:49:50 +0800

    清除e.print

--- src/main/java/com/asiainfo/tag/utils/ApplicationStartup.java
+++ src/main/java/com/asiainfo/tag/utils/ApplicationStartup.java
@@ -15,29 +15,29 @@ import org.springframework.context.event.ContextRefreshedEvent;
 public class ApplicationStartup implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-//        ApplicationContext ac = event.getApplicationContext();
-//        log.warn("=============================项目初始化工作  begin =============================");
-//        try {
-//            Connection connection = HbaseConnection.getInstance().getConnection();
-//            if(connection!=null && connection.getAdmin()!=null){
-//                log.warn("初始化生产集群连接成功");
-//            }else{
-//                log.error("初始化生产集群连接失败");
-//            }
-//        }catch (Exception e){
-//            log.error("初始化生产集群连接失败",e);
-//        }
-//
-//        try {
-//            Connection connection = HbaseServiceConnection.getInstance().getConnection();
-//            if(connection!=null && connection.getAdmin()!=null){
-//                log.warn("初始化襄阳服务集群连接成功");
-//            }else{
-//                log.error("初始化襄阳服务集群连接失败");
-//            }
-//        }catch (Exception e){
-//            log.error("初始化襄阳服务集群连接失败",e);
-//        }
-//        log.warn("=============================项目初始化工作  end  =============================");
+        ApplicationContext ac = event.getApplicationContext();
+        log.warn("=============================项目初始化工作  begin =============================");
+        try {
+            Connection connection = HbaseConnection.getInstance().getConnection();
+            if(connection!=null && connection.getAdmin()!=null){
+                log.warn("初始化生产集群连接成功");
+            }else{
+                log.error("初始化生产集群连接失败");
+            }
+        }catch (Exception e){
+            log.error("初始化生产集群连接失败",e);
+        }
+
+        try {
+            Connection connection = HbaseServiceConnection.getInstance().getConnection();
+            if(connection!=null && connection.getAdmin()!=null){
+                log.warn("初始化襄阳服务集群连接成功");
+            }else{
+                log.error("初始化襄阳服务集群连接失败");
+            }
+        }catch (Exception e){
+            log.error("初始化襄阳服务集群连接失败",e);
+        }
+        log.warn("=============================项目初始化工作  end  =============================");
     }
 }
